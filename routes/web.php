ecf10ce07bd382eadde3d13e416eca3051380ccc
abdelrahman <eng.abdo.com@gmail.com>
2025-07-26 21:20:16 +0300

    clear web file

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\Frontend\AuthController;
 use App\Http\Controllers\Frontend\AccountController;
 use App\Http\Controllers\Frontend\PagesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 
 Route::group(
 [
